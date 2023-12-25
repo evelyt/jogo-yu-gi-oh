28d9d7b92916db11ac7fe70eead39de48dc3a473
evelyt <terezaevely@gmail.com>
2023-12-25 13:25:32 -0300

    projeto criado

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -207,6 +207,10 @@ function init() {
     showHiddenCardFieldsImages(false);
     drawCards(5, state.playerSides.player1);
     drawCards(5, state.playerSides.computer);
+
+    const bgm = document.getElementById("bgm");
+    bgm.play();
+
 }
 
 init();
